0e5e479c774ea131fefbb0b8b3780b6e58f817f6
matheus freitas <mgsoutodelira@gmail.com>
2025-05-08 14:10:56 -0300

    criado rotas de auth

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+// Configuração da instância do Axios
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+const login = async (email: string, password: string): Promise<any> => {
+  try {
+    const response = await api.post("/auth/login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || error.message;
+  }
+};
+
+const register = async (name: string, email: string, password: string): Promise<any> => {
+  try {
+    const response = await api.post("/auth/register", {
+      name,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || error.message;
+  }
+};
+
+export { login, register };
