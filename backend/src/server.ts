0ede5cfe1bbc0c09edc1e00f1fb5324fdf16d352
matheus freitas <mgsoutodelira@gmail.com>
2025-05-08 14:43:17 -0300

    add solução para cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRouter from "./routes/authRoutes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: "http://localhost:3001",
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  credentials: true
+}));
 
 app.use(express.json());
 
