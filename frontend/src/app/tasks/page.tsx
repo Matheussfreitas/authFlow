f78854812a038df922cefc38189215f1ae3d76ac
Matheus Freitas <mgsoutodelira@gmail.com>
2025-05-11 22:06:18 -0300

    update responsividade

--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -4,32 +4,99 @@ import { useEffect, useState } from "react"
 import FilterSession from "./components/filters"
 import Header from "./components/header"
 import TasksList from "./components/tasksList"
-import { Task } from "@/types/Task"
+import { Task, TaskPriority, TaskStatus } from "@/types/Task"
 import { parseCookies } from "nookies"
-import { getUserByToken } from "@/utils/axios"
+import { deleteTask, getUserByToken, updateTask } from "@/utils/axios"
 import { User } from "@/types/User"
 
 export default function TaskPage() {
   const [user, setUser] = useState<User | null>(null);
-  const [tasks, setTasks] = useState<Task[] | []>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [statusFilter, setStatusFilter] = useState<"todas" | "concluídas" | "pendentes">("todas");
+  const [priorityFilter, setPriorityFilter] = useState<"todas" | "baixa" | "média" | "alta" | "urgente">("todas");
 
   const addTask = (task: Task) => {
     setTasks((prevTasks) => [...prevTasks, task]);
   }
 
-  const updateTask = (taskId: string) => {
+  const handleTaskComplete = async (taskId: string) => {
     const taskToUpdate = tasks.find((task) => task.id === taskId);
-    if (!taskToUpdate) {
-      throw new Error("Task not found");
+
+    if (!taskToUpdate) return;
+
+    const updatedTask = {
+      ...taskToUpdate,
+      status:
+        taskToUpdate.status === TaskStatus.PENDENTE
+          ? TaskStatus.CONCLUIDO
+          : TaskStatus.PENDENTE,
+    };
+
+    try {
+      const response = await updateTask(updatedTask.id, updatedTask);
+      console.log("Tarefa atualizada com sucesso:", response);
+
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === updatedTask.id ? updatedTask : task
+        )
+      );
+    } catch (error) {
+      console.error("Erro ao atualizar o status da tarefa:", error);
     }
-    return taskToUpdate;
   }
+
+  const handleUpdateTask = async (updatedTask: Task) => {
+    console.log("Tarefa atualizada recebida: ", updatedTask);
+
+    try {
+      const response = await updateTask(updatedTask.id, updatedTask);
+      console.log("Resposta da API: ", response);
+
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === updatedTask.id ? updatedTask : task
+        )
+      );
+
+      return response;
+    } catch (error) {
+      console.error("Erro ao atualizar a tarefa:", error);
+    }
+  }
+
+  const handleDeleteTask = async (taskId: string) => {
+    try {
+      const trash = await deleteTask(taskId);
+      console.log("Resposta da API: ", trash);
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+      console.log(`Tarefa com ID ${taskId} removida com sucesso.`);
+    } catch (error) {
+      console.error("Erro ao excluir a tarefa:", error);
+    }
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    const matchesStatus =
+      statusFilter === "todas" ||
+      (statusFilter === "concluídas" && task.status === TaskStatus.CONCLUIDO) ||
+      (statusFilter === "pendentes" && task.status === TaskStatus.PENDENTE);
+
+    const matchesPriority =
+      priorityFilter === "todas" ||
+      (priorityFilter === "urgente" && task.priority === TaskPriority.URGENTE) ||
+      (priorityFilter === "alta" && task.priority === TaskPriority.ALTA) ||
+      (priorityFilter === "média" && task.priority === TaskPriority.MEDIA) ||
+      (priorityFilter === "baixa" && task.priority === TaskPriority.BAIXA);
+
+    return matchesStatus && matchesPriority;
+  });
 
   useEffect(() => {
     const fetchUserInfo = async () => {
-      const cookies = await parseCookies().authFlowToken;
-      console.log(cookies);
-      const userInfo = await getUserByToken(cookies);
+      const token = await parseCookies().authFlowToken;
+      console.log("Token: ", token);
+      const userInfo = await getUserByToken(token);
       console.log("Informações do usuário obtidas: ", userInfo);
       setUser(userInfo);
       setTasks(userInfo.Task);
@@ -39,14 +106,25 @@ export default function TaskPage() {
   }, [])
 
   return (
-    <div className="container mx-auto px-4 py-8 max-w-4xl">
-
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 max-w-7xl">
       <Header userName={user?.name || ""} />
 
-      <FilterSession tasks={tasks} userId={user?.id} onAddTask={addTask}/>
+      <FilterSession
+        tasks={tasks}
+        userId={user?.id}
+        onAddTask={addTask}
+        statusFilter={statusFilter}
+        setStatusFilter={setStatusFilter}
+        priorityFilter={priorityFilter}
+        setPriorityFilter={setPriorityFilter}
+      />
 
-      <TasksList tasks={tasks} onEditTask={updateTask}/>
-
+      <TasksList
+        tasks={filteredTasks}
+        onEditTask={handleUpdateTask}
+        onCompleteTask={handleTaskComplete}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
-  )
+  );
 }
